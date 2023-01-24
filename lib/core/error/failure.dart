efadc06cc50c7539ec3164f25fd516067c88c1f1
sergeysokolov58 <sergeysokolov1958@gmail.com>
2023-01-24 23:58:17 +0200

    L5

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -6,3 +6,16 @@ abstract class Failure extends Equatable {
 
   const Failure([List properties = const <dynamic>[]]);
 }
+
+// General failures
+class ServerFailure extends Failure {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
+
+class CacheFailure extends Failure {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
